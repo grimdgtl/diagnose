b1fb77521bcc66b33acbf7272bc4dcb57fb8501c
Nikola Milic <milic@192.168.1.4>
2025-02-05 00:01:28 +0100

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import copy from 'vite-plugin-copy';
+import { copy } from 'vite-plugin-copy';
 
 export default defineConfig({
     plugins: [
