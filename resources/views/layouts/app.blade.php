9745fed45caed61c10d3dd7bf8be300a526c336d
Nikola Milic <milic@192.168.1.4>
2025-02-04 23:15:26 +0100

    inserted meta csrf token to head

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Diagnose App</title>
     <!-- Uključivanje CSS i JS preko Vite -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
