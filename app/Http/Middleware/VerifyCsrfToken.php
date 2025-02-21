329070bda11cd36b110b6b166ad7270009ffb84a
Nikola Milic <milic@Nikolas-MacBook-Pro.local>
2025-02-21 13:16:56 +0100

    bug fix

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class VerifyCsrfToken extends Middleware
 {
     protected $except = [
-        'lemon-squeezy/*', // Isključi webhook iz CSRF zaštite
+        'lemon-squeezy-webhook',
     ];
+
+    public function handle($request, Closure $next)
+    {
+        \Log::info('VerifyCsrfToken middleware triggered for path: ' . $request->getPathInfo());
+        return parent::handle($request, $next);
+    }
 }
