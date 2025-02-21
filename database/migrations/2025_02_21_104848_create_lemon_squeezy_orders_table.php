004794df1e27d5d1f998a55f514ad057523800d5
Nikola Milic <milic@Nikolas-MacBook-Pro.local>
2025-02-21 12:45:22 +0100

    bug fix

--- database/migrations/2025_02_21_104848_create_lemon_squeezy_orders_table.php
+++ database/migrations/2025_02_21_104848_create_lemon_squeezy_orders_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('lemon_squeezy_orders', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('billable_type');
-            $table->unsignedBigInteger('billable_id');
+            $table->string('billable_id');
             $table->string('lemon_squeezy_id');
             $table->string('customer_id');
             $table->char('identifier', 36);
